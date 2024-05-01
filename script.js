7b05ea55b92f1996c8491b16d5af2d5c58c233f8
Megan Williams <mwilliams1.prsvr@gmail.com>
2024-04-30 20:14:55 -0500

    added button

--- script.js
+++ script.js
@@ -9,23 +9,45 @@
 let message = ""
 
 let firstCard = 10
-let secondCard = 7
+let secondCard = 4
 
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🤗"
-} else if (sum === 21) {
-    message = "wohoo! You've got Blackjack! 😁"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😊"
-    isAlive = false
+let messageEl = document.getElementById("message-el")
+
+let sumEl = document.getElementById("sum-el")
+// let sumEl = document.querySelector("sum-el")
+
+let cardsEl = document.getElementById("cards-el")
+
+function startGame() {
+    sumEl.textContent = "Sum: " + sum
+cardsEl.textContent = "Cards: " + firstCard + " " + secondCard 
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    
+    
+    
+   messageEl.textContent = message
+
+    
+}
+
+function newCard() {
+
+console.log("Drawing a new card from the deck!")
+
 }
 
 
-console.log(isAlive)
-console.log(message)
 
