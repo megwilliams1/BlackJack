fcfbd0f7f63a54423a2860bd54529313e1d0fc6b
Megan Williams <mwilliams1.prsvr@gmail.com>
2024-05-01 19:53:55 -0500

    incomplete

--- script.js
+++ script.js
@@ -7,46 +7,48 @@
 // console.log(3 <= 2) false
 
 let message = ""
-
 let firstCard = 10
 let secondCard = 4
-
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
-
 let messageEl = document.getElementById("message-el")
-
 let sumEl = document.getElementById("sum-el")
 // let sumEl = document.querySelector("sum-el")
-
 let cardsEl = document.getElementById("cards-el")
 
+
+
 function startGame() {
+    renderGame()
+}
+
+function renderGame() {
     sumEl.textContent = "Sum: " + sum
-cardsEl.textContent = "Cards: " + firstCard + " " + secondCard 
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1] 
+    
+        if (sum <= 20) {
+            message = "Do you want to draw a new card?"
+        } else if (sum === 21) {
+            message = "You've got Blackjack!"
+            hasBlackJack = true
+        } else {
+            message = "You're out of the game!"
+            isAlive = false
+        }
+        
+        
+        
+       messageEl.textContent = message
 
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game!"
-        isAlive = false
-    }
-    
-    
-    
-   messageEl.textContent = message
-
-    
+       console.log("Drawing a new card from the deck!")
 }
 
 function newCard() {
-
-console.log("Drawing a new card from the deck!")
-
+let card = 7
+sum += card
+renderGame()
 }
 
 
